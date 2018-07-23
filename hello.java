d8aeeaa14d6e83254b3ac3885097752f1f0b577f
tansky <tansky@qq.com>
2018-07-23 21:40:19 +0800

    是我令狐冲修改的

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 		System.out.println("hello Git,I am tanshijie");
  HEAD
 		//i am hotfix
-
+			//这是我令狐冲写的
 		//wo shi master
  master
 		System.out.println("This my second commit data+++++++");
