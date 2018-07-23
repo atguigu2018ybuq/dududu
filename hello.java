0b49b7dfadbd6811adfe6e4631f59d72a6ccc88c
tansky <tansky@qq.com>
2018-07-23 20:16:09 +0800

    这是新建的分支hotfix提交的

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ public class hello{
 	public static void main(String[] args){
 		System.out.println("hello Git,I am tanshijie");
 
-		System.out.println("This my second commit data");
+		System.out.println("This my second commit data+++++++");
+		System.out.println("edit by hotfix");
 	}
 
 }
