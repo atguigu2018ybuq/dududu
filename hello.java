d8b4ca14ed9adea80731fa6bc2787cd0f12af4cd
tansky <tansky@qq.com>
2018-07-23 20:39:20 +0800

    master准备冲突

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("hello Git,I am tanshijie");
-
+		//wo shi master
 		System.out.println("This my second commit data+++++++");
 		System.out.println("edit by hotfix");
 	}
